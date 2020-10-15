6ba6bf21e2aede1771905d9041402d8ea9b28179
Jonhks <jonhgx@gmail.com>
2020-10-15 15:47:16 -0500

    Updates

---
+++ precache-manifest.54d6a0f570169fb281e5f6fae5f0bacd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6dc0ae03a96f2adc986e419dd4229b",
+    "url": "/prueba-tecnica/index.html"
+  },
+  {
+    "revision": "b0b25e141a45cff8c91d",
+    "url": "/prueba-tecnica/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "c361a30780f5fde649f1",
+    "url": "/prueba-tecnica/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0b25e141a45cff8c91d",
+    "url": "/prueba-tecnica/static/js/2.8ddaa8b2.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/prueba-tecnica/static/js/2.8ddaa8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c361a30780f5fde649f1",
+    "url": "/prueba-tecnica/static/js/main.4fb2f45e.chunk.js"
+  },
+  {
+    "revision": "93a0bde759a9bd70a9a1",
+    "url": "/prueba-tecnica/static/js/runtime-main.3e157429.js"
+  }
+]);
